b69ee6933c21882e3982a8e7ab4671f65ba9bd84
sbenard <sbenard@mtwin.fr>
2018-12-12 23:55:45 +0100

    Shorter redist bat

--- makeRedist.bat
+++ makeRedist.bat
@@ -5,58 +5,47 @@ set game=SaveAndSacrifice
 
 echo Cleaning...
 rmdir /Q/S "%redist%" >nul
-REM mkdir "%redist%\"
-mkdir "%redist%\%game%\"
-REM del "%redist%\*.zip"  >nul 2>&1
-REM del "%redist%\*.swf"  >nul 2>&1
-REM del "%redist%\*.js"  >nul 2>&1
+mkdir "%redist%\" >nul
+mkdir "%redist%\%game%\" >nul
 
 echo Copying hl.exe files...
 copy %haxepath%\hl.exe "%redist%\%game%"
-
-copy %haxepath%\chroma.hdll "%redist%\%game%"
-copy %haxepath%\directx.hdll "%redist%\%game%"
-copy %haxepath%\fmt.hdll "%redist%\%game%"
-copy %haxepath%\freetype.hdll "%redist%\%game%"
-copy %haxepath%\openal.hdll "%redist%\%game%"
-copy %haxepath%\sdl.hdll "%redist%\%game%"
-copy %haxepath%\sqlite.hdll "%redist%\%game%"
-copy %haxepath%\ssl.hdll "%redist%\%game%"
-copy %haxepath%\ui.hdll "%redist%\%game%"
-copy %haxepath%\uv.hdll "%redist%\%game%"
-
-copy %haxepath%\depends.dll "%redist%\%game%"
-copy %haxepath%\gc.dll "%redist%\%game%"
-copy %haxepath%\libcurl.dll "%redist%\%game%"
 copy %haxepath%\libhl.dll "%redist%\%game%"
-copy %haxepath%\libpcre-1.dll "%redist%\%game%"
-copy %haxepath%\msvcp120.dll "%redist%\%game%"
-copy %haxepath%\msvcr100.dll "%redist%\%game%"
 copy %haxepath%\msvcr120.dll "%redist%\%game%"
-copy %haxepath%\neko.dll "%redist%\%game%"
 copy %haxepath%\OpenAL32.dll "%redist%\%game%"
 copy %haxepath%\SDL2.dll "%redist%\%game%"
-copy %haxepath%\zlib1.dll "%redist%\%game%"
+REM copy %haxepath%\depends.dll "%redist%\%game%"
+REM copy %haxepath%\gc.dll "%redist%\%game%"
+REM copy %haxepath%\msvcr100.dll "%redist%\%game%"
+REM copy %haxepath%\msvcp120.dll "%redist%\%game%"
+REM copy %haxepath%\neko.dll "%redist%\%game%"
 
-copy %haxepath%\ssl.ndll "%redist%\%game%"
-copy %haxepath%\std.ndll "%redist%\%game%"
-copy %haxepath%\zlib.ndll "%redist%\%game%"
-copy %haxepath%\regexp.ndll "%redist%\%game%"
-copy %haxepath%\mysql5.ndll "%redist%\%game%"
-copy %haxepath%\mod_tora2.ndll "%redist%\%game%"
+copy %haxepath%\fmt.hdll "%redist%\%game%"
+copy %haxepath%\openal.hdll "%redist%\%game%"
+copy %haxepath%\ui.hdll "%redist%\%game%"
+copy %haxepath%\uv.hdll "%redist%\%game%"
+copy %haxepath%\sdl.hdll "%redist%\%game%"
+REM copy %haxepath%\ssl.ndll "%redist%\%game%"
+REM copy %haxepath%\std.ndll "%redist%\%game%"
+REM copy %haxepath%\zlib.ndll "%redist%\%game%"
+REM copy %haxepath%\regexp.ndll "%redist%\%game%"
+REM copy %haxepath%\mysql5.ndll "%redist%\%game%"
+REM copy %haxepath%\mod_tora2.ndll "%redist%\%game%"
+
+
+ren "%redist%\%game%"\*.exe "%game%.exe"
 
 echo HL...
 haxe hl.hxml
 
-REM REM echo Flash...
-REM REM haxe flash.hxml
+echo Flash...
+haxe flash.hxml
 
 echo JS...
 haxe js.hxml
 
 echo Copying binaries...
 copy bin\client.hl "%redist%\%game%\hlboot.dat" >nul
-REM copy bin\client.swf "%redist%"" >nul
+copy bin\client.swf "%redist%" >nul
 copy bin\client.js "%redist%" >nul
-ren "%redist%\%game%"\*.exe "%game%.exe"
 echo Done!
